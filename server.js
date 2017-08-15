6d8b9b14c642514bb969eef84706bab0812cc21f
LV Magee <lvmagee@gmail.com>
2017-08-14 23:12:05 -0400

    Added mongoldb key for heroic

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static("./public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb://localhost/nytsearchmong");
+mongoose.connect("mongodb://heroku_2dhbggsv:m74fpjkgrkt666tr32203elv33@ds155841.mlab.com:55841/heroku_2dhbggsv");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
